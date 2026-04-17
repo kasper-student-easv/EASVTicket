d416eb3bf1f0da297c396af05a4488b2235e9794
Kasper <kasand04@easv365.dk>
2026-04-17 08:44:15 +0200

    fix

--- src/main/java/dk/easv/easvticket/EventDetails.java
+++ src/main/java/dk/easv/easvticket/EventDetails.java
@@ -1,4 +1,22 @@
 package dk.easv.easvticket;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class EventDetails {
+    @FXML
+    private void onAddCoordinator(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onDelete(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onEdit(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onClose(ActionEvent actionEvent) {
+    }
 }
